af06eda2b54f5fe6d090894b5d9a6b451cd06d7b
Priyanka Mishra <mishra.priyanka3399@gmail.com>
2026-08-13 19:16:11 +0530

    2.1 change sub defn

--- math_utils.py
+++ math_utils.py
@@ -4,11 +4,11 @@ def add(a, b):
 
 def sub(a, b):
     """Return the difference of a and b."""
-    return a - b
+    return add(a, -b)
 
 if __name__ == "__main__":
     result_add = add(3, 5)
-    print(f"SUM: {result}")
+    print(f"SUM: {result_add}")
  
     result_sub = sub(10, 4)
     print(f"DIFFERENCE: {result_sub}")
